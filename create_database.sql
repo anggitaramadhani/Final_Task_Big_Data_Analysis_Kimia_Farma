4dea5925cd649ce4ca5f51533b03d82ffc9e1d15
anggitaramadhani <55416768+anggitaramadhani@users.noreply.github.com>
2023-12-04 19:04:10 +0700

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -3,3 +3,4 @@ CREATE DATABASE kimia_farma;
 USE kimia_farma;
 
 -- untuk isi dari datanya, saya langsung import manual ke dalam dbeavernya.
+-- untuk data yang diimport ada 'pelanggan.csv', 'penjualan.csv', dan 'barang.csv'
